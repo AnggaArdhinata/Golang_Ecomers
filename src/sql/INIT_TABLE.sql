8ae2979506b34c50e3a3ee9b52edc762d1be82a6
AnggaArdhinata <ardhinataangga@gmail.com>
2023-06-19 16:46:37 +0700

    fix sql

--- src/sql/INIT_TABLE.sql
+++ src/sql/INIT_TABLE.sql
@@ -1,18 +1,18 @@
--- SET TIMEZONE ='Asia/Jakarta';
+SET TIMEZONE ='Asia/Jakarta';
 CREATE TABLE IF NOT EXISTS customer (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR NOT NULL,
     email VARCHAR,
     password VARCHAR,
-    created_at TIMESTAMPZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPZ
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS categories (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR NOT NULL,
-    created_at TIMESTAMPZ NOT NULL,
-    updated_at TIMESTAMPZ  
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP  
 );
 
 CREATE TABLE IF NOT EXISTS product (
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS product (
     price INT,
     description TEXT,
     image VARCHAR,
-    created_at TIMESTAMPZ NOT NULL,
-    updated_at TIMESTAMPZ,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT product_fk_1 FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
@@ -34,8 +34,8 @@ CREATE TABLE IF NOT EXISTS orders (
     ispaid BOOLEAN default false,
     status VARCHAR,
     discount_code VARCHAR,
-    created_at TIMESTAMPZ NOT NULL,
-    updated_at TIMESTAMPZ,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     CONSTRAINT orderfk_1 FOREIGN KEY (cust_id) REFERENCES customer(id) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT orderfk_2 FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
